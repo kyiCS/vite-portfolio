b472c60e9f4b641906f9088aa53d2294c2aa02ff
Kelvin <kyipersonal@gmail.com>
2024-04-11 18:00:57 -0400

    gradient work

--- src/components/GradientBackground/GradientBackground.jsx
+++ src/components/GradientBackground/GradientBackground.jsx
@@ -5,10 +5,10 @@ export const GradientBackground = () => {
   return (
     <section className={styles.gradientBackground}>
         <div className={styles.gradientContainer}>
-            
 
             <div className={styles.topLeft}></div>
-            <div className={styles.topMiddle}></div>
+            <div className={styles.topRight}></div>
+            <div className={styles.topCenter}></div>
 
 
 
